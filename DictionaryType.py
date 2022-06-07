55fc2a9f569500a7b4fa1a4833097b2918cad23f
Raghav Jeksani <raghavsqa@gmail.com>
2022-06-07 16:27:44 -0500

    new types

---
+++ DictionaryType.py
@@ -0,0 +1,13 @@
+# Dictionary type is similar to the Key Value pair like Hashmap or Table with multiple data types in single dictionary
+# denoted using flower braces
+
+dict = {1:"laddu" , 2:"pandu", 3:"raghav" , "last":4}
+
+print(dict[1])
+print(dict[2])
+print(dict[3])
+print(dict["last"])
+
+dict[5] = "kris"  # inserting values into a dictionary
+
+print(dict[5])
---
+++ TupleDataType.py
@@ -0,0 +1,8 @@
+# this is similar to list, but cannot be modified (updated - inserted or deleted at runtime). "(" is used to assign
+
+sampleList = (1, 2, "raghav", 0.089) # Multiple data types in a single tuple
+
+print(sampleList)
+print(sampleList[3])  # printing an index
+print(sampleList[-1])   # last index is printed
+
